132b1f5be8ac9a89c36af043634d3905fc3a7cf1
João Rodrigues <j.p.g.l.m.rodrigues@gmail.com>
2021-02-01 17:20:42 -0800

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Dotfiles Repository
+A simple way to version-control important settings.
+
+## Usage
+1. Create/Edit a file in the `files/` folder.
+2. Edit the `dotfile.loc` file to map files to locations.
+3. Run the `install.sh` script.
---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Read mappings in dotfile.loc and copy files.
+while read line
+do
+  # Cut by multiple whitespace
+  fname=$( echo $line | tr -s ' ' | cut -d ' ' -f 1 )
+  floc=$( echo $line | tr -s ' ' | cut -d ' ' -f 2 )
+
+  # evaluate floc in case of variable (e.g. $HOME)
+  if [[ $floc =~ "$" ]]
+  then
+      floc=$( eval echo $floc )
+  fi
+
+  echo "copying $fname ..."
+  /bin/cp -f files/${fname} $floc
+done < dotfile.loc
+
+echo "Success."
+exit 0
